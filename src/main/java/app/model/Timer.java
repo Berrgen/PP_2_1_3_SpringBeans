f1bf17c3ee1e1b5475c926245dccaeaa0a4746c2
Berrgen <sergeu.css@gmail.com>
2022-08-16 09:59:36 +0300

    add dog and timer

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,21 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
+    @Autowired
+    private Timer timer;
 
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
